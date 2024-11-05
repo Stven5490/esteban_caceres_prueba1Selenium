7b36b87a0a2486bd2934f766e22444ee43492bcd
Stven5490 <est.caceres@gmail.com>
2024-11-05 17:14:55 -0300

    Carga 5 casos

--- src/test/java/testEdwards.java
+++ src/test/java/testEdwards.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,6 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Random;
 
-
 public class testEdwards {
     private static WebDriver driver;
     private String rutaDriver = System.getProperty("user.id") + "\\src\\test\\resources\\drivers\\chromedriver.exe";
@@ -25,7 +25,8 @@ public class testEdwards {
     }
     @Test
     public void CP001_llegarSitioEdwards(){
-        driver.get("https://portales.bancoedwards.cl/personas");
+        String urlBcoEdwards = "https://portales.bancoedwards.cl/personas";
+        driver.get(urlBcoEdwards);
         System.out.println("Se ingresar al sitio: " + driver.getTitle());
         System.out.println("Maximizando vista");
         driver.manage().window().maximize();
@@ -37,10 +38,15 @@ public class testEdwards {
         WebElement btnHazteCliente;
         By bybtnHazteCliente = By.xpath("//*[@id=\"pbec_header-link-hazte_cliente\"]");
         btnHazteCliente = driver.findElement(bybtnHazteCliente);
-        btnHazteCliente.click();
-        System.out.println("Clickeando en boton: " + driver.getTitle());
-        Thread.sleep(10000);
-
+        System.out.println("Validando si existe boton 'Hazte Cliente'.");
+        if (btnHazteCliente.isDisplayed()){
+            System.out.println("El boton si esta");
+            btnHazteCliente.click();
+            System.out.println("Clickeando en boton: " + driver.getTitle());
+            Thread.sleep(10000);
+        } else {
+            System.err.println("No se encuentra el boton.");
+        }
     }
     @Test
     public void CP003_hazteClienteok() throws InterruptedException {
@@ -61,7 +67,7 @@ public class testEdwards {
         WebElement Apellidos;
         By byApellidos = By.xpath("//*[@id=\"txt_apellido\"]");
         Apellidos = driver.findElement(byApellidos);
-        Apellidos.sendKeys("Cáceres Creuz");
+        Apellidos.sendKeys("Caceres Creuz");
         //Rut
         System.out.println("Ingresando RUT");
         WebElement Rut;
@@ -159,10 +165,137 @@ public class testEdwards {
         Assertions.assertEquals(resultadoEsperado, stringResultado);
     }
     @Test
-    public void hazteCliente() throws InterruptedException {
-        CP003_hazteClienteok();
+    public void CP004_hazteClienteRutinValido() throws InterruptedException {
+        CP002_hazteCliente();
+        //Formulario iFrame
+        WebElement iFrame;
+        By byiFrame = By.id("form_hazte_cliente");
+        iFrame = driver.findElement(byiFrame);
+        driver.switchTo().frame(iFrame);
+        //Nombres
+        System.out.println("Ingresando Nombres");
+        WebElement Nombres;
+        By byNombres = By.xpath("//*[@id=\"txt_nombre\"]");
+        Nombres = driver.findElement(byNombres);
+        Nombres.sendKeys("Esteban Orlando");
+        //Apellidos
+        System.out.println("Ingresando Apellidos");
+        WebElement Apellidos;
+        By byApellidos = By.xpath("//*[@id=\"txt_apellido\"]");
+        Apellidos = driver.findElement(byApellidos);
+        Apellidos.sendKeys("Caceres Creuz");
+        //Rut
+        System.out.println("Ingresando RUT");
+        WebElement Rut;
+        By byRut = By.xpath("//*[@id=\"txt_rut\"]");
+        Rut = driver.findElement(byRut);
+        Rut.sendKeys("11");
+        //Actividad
+        System.out.println("Seleccionando actividad");
+        By bycampoActividad = By.xpath("//*[@id='ddlActividad-selectized']");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(bycampoActividad)).click();
+        By opcionTrabajando = By.xpath("//*[contains(text(), 'Trabajando')]");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(opcionTrabajando)).click();
+        //Telefono
+        System.out.println("Indicando Nro. telefonico");
+        WebElement Telefono;
+        By byTelefono = By.xpath("//*[@id=\"txt_telefono\"]");
+        Telefono = driver.findElement(byTelefono);
+        Telefono.sendKeys("12345678");
+        //Email
+        System.out.println("Ingresando eMail");
+        WebElement Email;
+        By byEmail = By.xpath("//*[@id=\"txtEmail\"]");
+        Email = driver.findElement(byEmail);
+        Email.sendKeys("x@x.com");
+        //Siguiente
+        System.out.println("Data ingresada & validada");
+        System.out.println("Clickeando siguiente");
+        WebElement Siguiente;
+        By bySiguiente = By.xpath("//*[@id=\"bt_step\"]");
+        Siguiente = driver.findElement(bySiguiente);
+        Siguiente.click();
+        //Status Rut invalido
+        String resultadoEsperado = corregirEncoding("Por favor, ingrese un rut válido.");
+        String stringResultado = driver.findElement(By.xpath("/html/body/form/div[4]/div[1]/div[1]/div[3]/div/div/div/div[2]/label[1]")).getText();
+        Assertions.assertEquals(resultadoEsperado, stringResultado);
+        if (resultadoEsperado == stringResultado){
+            System.err.println("Rut ingresado es invalido.");
+        } else {
+            System.out.println(resultadoEsperado);
+        }
     }
-    public static String generarRUT() {
+    @Test
+    public void CP005_ingresarComunaInexistente() throws InterruptedException {
+        CP002_hazteCliente();
+        //Formulario iFrame
+        WebElement iFrame;
+        By byiFrame = By.id("form_hazte_cliente");
+        iFrame = driver.findElement(byiFrame);
+        driver.switchTo().frame(iFrame);
+        //Nombres
+        System.out.println("Ingresando Nombres");
+        WebElement Nombres;
+        By byNombres = By.xpath("//*[@id=\"txt_nombre\"]");
+        Nombres = driver.findElement(byNombres);
+        Nombres.sendKeys("Esteban Orlando");
+        //Apellidos
+        System.out.println("Ingresando Apellidos");
+        WebElement Apellidos;
+        By byApellidos = By.xpath("//*[@id=\"txt_apellido\"]");
+        Apellidos = driver.findElement(byApellidos);
+        Apellidos.sendKeys("Caceres Creuz");
+        //Rut
+        System.out.println("Ingresando RUT");
+        WebElement Rut;
+        By byRut = By.xpath("//*[@id=\"txt_rut\"]");
+        Rut = driver.findElement(byRut);
+        Rut.sendKeys(generarRUT());
+        //Actividad
+        System.out.println("Seleccionando actividad");
+        By bycampoActividad = By.xpath("//*[@id='ddlActividad-selectized']");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(bycampoActividad)).click();
+        By opcionTrabajando = By.xpath("//*[contains(text(), 'Trabajando')]");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(opcionTrabajando)).click();
+        //Telefono
+        System.out.println("Indicando Nro. telefonico");
+        WebElement Telefono;
+        By byTelefono = By.xpath("//*[@id=\"txt_telefono\"]");
+        Telefono = driver.findElement(byTelefono);
+        Telefono.sendKeys("12345678");
+        //Email
+        System.out.println("Ingresando eMail");
+        WebElement Email;
+        By byEmail = By.xpath("//*[@id=\"txtEmail\"]");
+        Email = driver.findElement(byEmail);
+        Email.sendKeys("x@x.com");
+        //Siguiente
+        System.out.println("Data ingresada & validada");
+        System.out.println("Clickeando siguiente");
+        WebElement Siguiente;
+        By bySiguiente = By.xpath("//*[@id=\"bt_step\"]");
+        Siguiente = driver.findElement(bySiguiente);
+        Siguiente.click();
+        //Comuna
+        System.out.println("Seleccionando Comuna");
+        By byComuna = By.xpath("//*[@id=\"ddl_comunas-selectized\"]");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(byComuna)).click();
+            By byComunaOpcion = By.xpath("//[contains(text(), 'Tangamandapio')]");
+            if (!driver.findElements(byComunaOpcion).isEmpty()){
+                driver.findElement(byComunaOpcion).click();
+                System.out.println("Comuna indicada existe");
+            } else {
+                System.err.println("Comuna indicada no esta disponible");
+            }
+        } catch (Exception e){
+            System.out.println("Comuna Tangamandapio no esta disponible.");
+        }
+    }
+   public static String generarRUT() {
         Random random = new Random();
         int numeroBase = 1000000 + random.nextInt(9000000);
         int digitoVerificador = calcularDigitoVerificador(numeroBase);
